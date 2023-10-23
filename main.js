3394d032f7674acff9e5bc265947b26e25ce9313
molk915 <qwerkqwerk41@gmail.com>
2023-10-23 08:53:08 +0200

    pierwszy commit

---
+++ main.js
@@ -0,0 +1,2 @@
+const SongsData =  'https://gist.githubusercontent.com/techniadrian/c39f844edbacee0439bfeb107227325b/raw/81eec7847b1b3dfa1c7031586405c93e9a9c1a2d/songs.json'
+const GenresData = 'https://gist.githubusercontent.com/techniadrian/6ccdb1c837d431bb84c2dfedbec2e435/raw/60783ebfa89c6fd658aaf556b9f7162553ac0729/genres.json'
